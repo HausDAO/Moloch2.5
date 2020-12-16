99fc72f7a7326ee0da418668d70a5e257c48e2a1
Bill Warren <w.s.warren12@gmail.com>
2020-12-16 14:47:07 -0500

    Cleanup
    - Removes auto withdraw function for security reasons
    - Adds tokenWhitelist check into the cross withdraw and IERC20 transfer

--- MinionFactory.sol
+++ MinionFactory.sol
@@ -1,8 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.7.5;
 
+interface IERC20 { // brief interface for moloch erc20 token txs
+    function balanceOf(address who) external view returns (uint256);
+    
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    
+    function approve(address spender, uint256 amount) external returns (bool);
+}
+
 interface IMOLOCH { // brief interface for moloch dao v2
+
+
     function depositToken() external view returns (address);
+    
+    function tokenWhitelist(address token) external view returns (bool);
     
     function getProposalFlags(uint256 proposalId) external view returns (bool[6] memory);
 
@@ -38,6 +52,7 @@ contract Minion {
     event ExecuteAction(uint256 proposalId, address executor);
     event DoWithdraw(address token, uint256 amount);
     event CrossWithdraw(address target, address token, uint256 amount);
+    event PulledFunds(address moloch, uint256 amount);
 
     function init(address _moloch) external {
         require(!initialized, "initialized"); 
@@ -52,16 +67,16 @@ contract Minion {
     }
     
     function crossWithdraw(address target, address token, uint256 amount) external {
-
-        /* 
-        Target needs to have a withdrawBalance function that uses the same
-        name and inputs as the withdrawBalance in a Moloch -- so probably other Molochs. 
-        */ 
-        
+        // @Dev - Target needs to have a withdrawBalance functions
+        bool whitelisted = moloch.tokenWhitelist(token);
+        require(whitelisted, "not a whitelisted token");
         IMOLOCH(target).withdrawBalance(token, amount); 
+        
+        // Transfers token into DAO. 
+        IERC20(token).transfer(address(moloch), amount);
         emit CrossWithdraw(target, token, amount);
     }
-
+    
     function proposeAction(
         address actionTo,
         uint256 actionValue,
@@ -155,11 +170,11 @@ contract MinionFactory is CloneFactory {
     address[] public minionList; 
     mapping (address => AMinion) public minions;
     
-    event SummonMinion(address indexed minion, address indexed moloch, string name, string minionType);
+    event SummonMinion(address indexed minion, address indexed moloch, string details, string minionType);
     
     struct AMinion {
         address moloch;
-        string name; 
+        string details; 
     }
     
     
@@ -167,15 +182,15 @@ contract MinionFactory is CloneFactory {
         template = _template;
     }
     
-    function summonMinion(address moloch, string memory name) external returns (address) {
+    function summonMinion(address moloch, string memory details) external returns (address) {
         Minion minion = Minion(createClone(template));
         
         minion.init(moloch);
         string memory minionType = "vanilla minion";
         
-        minions[address(minion)] = AMinion(moloch, name);
+        minions[address(minion)] = AMinion(moloch, details);
         minionList.push(address(minion));
-        emit SummonMinion(address(minion), moloch, name, minionType);
+        emit SummonMinion(address(minion), moloch, details, minionType);
         
         return(address(minion));
         
