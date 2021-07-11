e52d9bd792645f2dc14f1c2359dfff2424ab9c9d
Isaac Patka <ipatka@gmail.com>
2021-07-11 12:19:45 -0400

    wip call stack

--- contracts/EscrowMinion.sol
+++ contracts/EscrowMinion.sol
@@ -3,38 +3,98 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.7.5;
 
-interface IERC20 { // brief interface for moloch erc20 token txs
+interface IERC20 {
+    // brief interface for moloch erc20 token txs
     function balanceOf(address who) external view returns (uint256);
-    
+
     function transfer(address to, uint256 value) external returns (bool);
 
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
-    
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external returns (bool);
+
     function approve(address spender, uint256 amount) external returns (bool);
 }
 
-interface IERC721 { // brief interface for minion erc721 token txs
-    function safeTransferFrom(address from, address to, uint256 tokenId) external;
+interface IERC721 {
+    // brief interface for minion erc721 token txs
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
 }
 
-// TODO add IERC1155Receiver
+interface IERC1155 {
+    // brief interface for minion erc1155 token txs
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes calldata data
+    ) external;
+}
+
 interface IERC721Receiver {
-    function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data) external returns (bytes4);
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4);
 }
 
-interface IMOLOCH { // brief interface for moloch dao v2
+interface IERC1155Receiver {
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external returns (bytes4);
 
+    // TODO batch receive not implemented in tribute yet
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] calldata ids,
+        uint256[] calldata values,
+        bytes calldata data
+    ) external returns (bytes4);
+}
+
+interface IMOLOCH {
+    // brief interface for moloch dao v2
 
     function depositToken() external view returns (address);
-    
+
     function tokenWhitelist(address token) external view returns (bool);
-    
-    function getProposalFlags(uint256 proposalId) external view returns (bool[6] memory);
-    
-    function members(address user) external view returns (address, uint256, uint256, bool, uint256, uint256);
-    
-    function userTokenBalances(address user, address token) external view returns (uint256);
-    
+
+    function getProposalFlags(uint256 proposalId)
+        external
+        view
+        returns (bool[6] memory);
+
+    function members(address user)
+        external
+        view
+        returns (
+            address,
+            uint256,
+            uint256,
+            bool,
+            uint256,
+            uint256
+        );
+
+    function userTokenBalances(address user, address token)
+        external
+        view
+        returns (uint256);
+
     function cancelProposal(uint256 proposalId) external;
 
     function submitProposal(
@@ -47,92 +107,153 @@ interface IMOLOCH { // brief interface for moloch dao v2
         address paymentToken,
         string calldata details
     ) external returns (uint256);
-    
+
     function withdrawBalance(address token, uint256 amount) external;
 }
 
 contract EscrowMinion is IERC721Receiver {
-    mapping(address => mapping (uint256 => TributeEscrowAction)) public actions; // proposalId => Action
+    mapping(address => mapping(uint256 => TributeEscrowAction)) public actions; // proposalId => Action
+
+    enum TributeType {
+        ERC721,
+        ERC20,
+        ERC1155
+    }
 
     struct TributeEscrowAction {
-        address tokenAddress;
-        uint256 tokenId;
-        address vaultAddress;
+        TributeType[] tributeTypes;
+        address[] tokenAddresses;
+        uint256[] tokenIds;
+        uint256[] amounts;
+        address vaultAddress; // todo multiple vault destinations?
         address proposer;
-        address applicant;
         bool executed;
     }
 
     event ProposeAction(uint256 proposalId, address proposer, address moloch);
     event ExecuteAction(uint256 proposalId, address executor, address moloch);
     event ActionCanceled(uint256 proposalId, address moloch);
-    
-    function onERC721Received (address, address, uint256, bytes calldata) external pure override returns(bytes4) {
-        return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
-    } 
-    
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external pure override returns (bytes4) {
+        return
+            bytes4(
+                keccak256("onERC721Received(address,address,uint256,bytes)")
+            );
+    }
+
+    function doTransfers(
+        TributeEscrowAction memory action,
+        address from,
+        address to
+    ) internal {
+        for (uint256 index = 0; index < action.tributeTypes.length; index++) {
+            if (action.tributeTypes[index] == TributeType.ERC721) {
+                IERC721 erc721 = IERC721(action.tokenAddresses[index]);
+                erc721.safeTransferFrom(from, to, action.tokenIds[index]);
+            } else if (action.tributeTypes[index] == TributeType.ERC20) {
+                IERC20 erc20 = IERC20(action.tokenAddresses[index]);
+                erc20.transferFrom(from, to, action.amounts[index]);
+            } else if (action.tributeTypes[index] == TributeType.ERC1155) {
+                IERC1155 erc1155 = IERC1155(action.tokenAddresses[index]);
+                erc1155.safeTransferFrom(
+                    from,
+                    to,
+                    action.tokenIds[index],
+                    action.amounts[index],
+                    ""
+                );
+            }
+        }
+    }
+
+    function saveAction(
+        address molochAddress,
+        // add array of erc1155, 721 or 20
+        TributeType[] calldata tributeTypes,
+        address[] calldata tokenAddresses,
+        uint256[] calldata tokenIds,
+        uint256[] calldata amounts,
+        address vaultAddress,
+        uint256 proposalId
+    ) private {
+        TributeEscrowAction memory action = TributeEscrowAction({
+            tributeTypes: tributeTypes,
+            tokenAddresses: tokenAddresses,
+            tokenIds: tokenIds,
+            amounts: amounts,
+            vaultAddress: vaultAddress,
+            proposer: msg.sender,
+            executed: false
+        });
+
+        actions[molochAddress][proposalId] = action;
+        doTransfers(action, msg.sender, address(this));
+    }
+
     //  -- Proposal Functions --
-   /**
+    /**
      * @notice Creates a proposal and moves NFT into escrow
-     * @param applicant Application address for the new shares
      * @param molochAddress Address of DAO
-     * @param tokenAddress NFT contract address
-     * @param tokenId NFT token id.
+     * @param tokenAddresses Token contract address
+     * @param tokenIds Token id.
+     * @param amounts Token amounts
      * @param vaultAddress Address of DAO's NFT vault
      * @param requestAmount Amount of shares requested
      * @param details Info about proposal
      */
     // todo no re-entrency
     function proposeTribute(
-        address applicant, // can remove
-        address molochAddress, 
+        address molochAddress,
         // add array of erc1155, 721 or 20
-        address tokenAddress,
-        uint256 tokenId,
+        TributeType[] calldata tributeTypes,
+        address[] calldata tokenAddresses,
+        uint256[] calldata tokenIds,
+        uint256[] calldata amounts,
         address vaultAddress,
         uint256 requestAmount, // also request loot or treasury funds
         string calldata details
-        ) external returns (uint256) {
-          IMOLOCH thisMoloch = IMOLOCH(molochAddress);
-          address thisMolochDepositToken = thisMoloch.depositToken();
-          
-          // Enforce applicant is message sender so proposal can't be front-run
-          require(applicant == msg.sender, 'sender is not applicant');
+    ) external returns (uint256) {
+        IMOLOCH thisMoloch = IMOLOCH(molochAddress);
+        address thisMolochDepositToken = thisMoloch.depositToken();
 
-          require(vaultAddress != address(0), "invalid vaultAddress");
-          
-          // TODO maybe do an interface check to see if vaultAddress is IERC721Receiver
-          
-          IERC721 erc721 = IERC721(tokenAddress);
-          
-          erc721.safeTransferFrom(applicant, address(this), tokenId);
+        require(vaultAddress != address(0), "invalid vaultAddress");
 
-          uint256 proposalId = thisMoloch.submitProposal(
-              applicant,
-              requestAmount,
-              0,
-              0,
-              thisMolochDepositToken,
-              0,
-              thisMolochDepositToken,
-              details
-          );
-          
-          TributeEscrowAction memory action = TributeEscrowAction({
-            tokenAddress: tokenAddress,
-            tokenId: tokenId,
-            vaultAddress: vaultAddress,
-            proposer: msg.sender,
-            applicant: applicant,
-            executed: false
-          });
-          
-          actions[molochAddress][proposalId] = action;
-          
-          emit ProposeAction(proposalId, msg.sender, molochAddress);
-          return proposalId;
-        }
-    
+        // require length check
+        require(tributeTypes.length == tokenAddresses.length, "!length");
+        require(tributeTypes.length == tokenIds.length, "!length");
+        require(tributeTypes.length == amounts.length, "!length");
+
+        uint256 proposalId = thisMoloch.submitProposal(
+            msg.sender,
+            requestAmount,
+            0,
+            0,
+            thisMolochDepositToken,
+            0,
+            thisMolochDepositToken,
+            details
+        );
+
+        // call stack too deep, try hashing the stuff instead
+        saveAction(
+            molochAddress,
+            tributeTypes,
+            tokenAddresses,
+            tokenIds,
+            amounts,
+            vaultAddress,
+            proposalId
+        );
+
+        emit ProposeAction(proposalId, msg.sender, molochAddress);
+        return proposalId;
+    }
+
     // todo no re-entrency
     function executeAction(uint256 proposalId, address molochAddress) external {
         IMOLOCH thisMoloch = IMOLOCH(molochAddress);
@@ -149,37 +270,33 @@ contract EscrowMinion is IERC721Receiver {
 
         require(!flags[3], "proposal cancelled");
 
-        // bool[6] memory flags; // [sponsored, processed, didPass, cancelled, whitelist, guildkick]
-
-        // if canceled or failed, return NFT to applicant
-        
-        IERC721 erc721 = IERC721(action.tokenAddress);
+        address destination;
         // if passed, send NFT to vault
-        if(flags[2]) {
-          erc721.safeTransferFrom(address(this), action.vaultAddress, action.tokenId);
+        if (flags[2]) {
+            destination = action.vaultAddress;
         } else {
-          erc721.safeTransferFrom(address(this), action.applicant, action.tokenId);
+            destination = action.proposer;
         }
+
+        doTransfers(action, address(this), destination);
 
         actions[molochAddress][proposalId].executed = true;
 
         emit ExecuteAction(proposalId, msg.sender, molochAddress);
     }
-    
+
     // todo no re-entrency
     function cancelAction(uint256 _proposalId, address molochAddress) external {
         IMOLOCH thisMoloch = IMOLOCH(molochAddress);
         TributeEscrowAction memory action = actions[molochAddress][_proposalId];
 
         require(msg.sender == action.proposer, "not proposer");
+        thisMoloch.cancelProposal(_proposalId);
+
+        doTransfers(action, address(this), msg.sender);
+
         delete actions[molochAddress][_proposalId];
 
-        //return the NFT
-        IERC721 erc721 = IERC721(action.tokenAddress);
-        erc721.safeTransferFrom(address(this), action.applicant, action.tokenId);
-
         emit ActionCanceled(_proposalId, molochAddress);
-        thisMoloch.cancelProposal(_proposalId);
     }
-    
 }
